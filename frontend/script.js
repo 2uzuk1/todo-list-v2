32c90ebc906c002f6c0508ec5312646ef108349a
2uzuk1 <g2uzuk1m@gmail.com>
2026-09-17 22:24:00 +0900

    feat: implement task deletion

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,30 @@
-const add_button = document.querySelector('button')
+const add_button = document.querySelector('#add-btn')
 const input_txt = document.querySelector('input')
 
 function addTaskToList(task) {
     const ul = document.querySelector('#task-list')
     const li = document.createElement('li')
-    li.className = "py-2 px-2"
+    li.className = "py-2 px-2 flex justify-between items-center"
     li.id = `task-${task.id}`
 
     const span = document.createElement('span')
     span.textContent = task.title
     span.className = "break-all max-w-[325px]"
 
-    li.appendChild(span)
-    ul.appendChild(li)
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'x'
+    deleteBtn.className = 'text-[#0891b2] bg-[#f5f3ff] rounded-md px-3 py-1 font-bold transition duration-300 ease-in-out hover:scale-110 active:scale-100 active:opacity-100'
+    deleteBtn.addEventListener('click', function() {
+        deleteTask(task.id)
+    })
+
+    const rightDiv = document.createElement('div')
+    rightDiv.className = 'flex items-center gap-2'
+    rightDiv.append(deleteBtn)
+
+    li.append(span)
+    li.append(rightDiv)
+    ul.append(li)
 }
 
 add_button.addEventListener('click', function() {
@@ -39,6 +51,15 @@ function loadTasks() {
         console.log(tasks)
 
         tasks.forEach(task => addTaskToList(task))
+    })
+}
+
+function deleteTask(id) {
+    fetch(`http://127.0.0.1:8000/tasks/${id}`, {
+        method: 'DELETE'
+    })
+    .then(() => {
+        document.querySelector(`#task-${id}`).remove()
     })
 }
 
